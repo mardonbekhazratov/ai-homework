400b1c0546c989fd6ee33b2e8d9735d498e84da0
Mardon Hazratov <mardonhazratov@Mardons-MacBook-Air.local>
2025-09-21 22:12:33 +0500

    add kmeans homework

--- ml/unsupervised/dimensionality_reduction/clustering/KMeans.py
+++ ml/unsupervised/dimensionality_reduction/clustering/KMeans.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class KMeans:
 
@@ -12,13 +13,31 @@ class KMeans:
 
     def fit(self, X, y = None):
         n_samples, n_features = X.shape
-        self.cluster_centers_ = np.random.randn()
+        self.cluster_centers_ = np.random.randn(self.n_clusters, n_features)
         for iter in range(self.max_iter):
-            pass
+            distances = np.stack([np.sqrt(np.sum((X - cluster_center)**2, axis=1)) \
+                                  for cluster_center in self.cluster_centers_], axis=1)
+            clusters = distances.argmin(axis=1)
+
+            means = []
+
+            for cluster in range(self.n_clusters):
+                mean = X[clusters == cluster].mean(axis=0)
+                means.append(mean)
+            
+            old_centroids = self.cluster_centers_
+            self.cluster_centers_ = np.stack(means, axis=0)
+
+            if np.sqrt(np.sum((self.cluster_centers_ - old_centroids)**2, axis=1)).max() < self.tol:
+                self.n_iters_ = iter + 1
+                break
+
 
     def predict(self, X):
-        pass
+        distances = np.stack([np.sqrt(np.sum((X - cluster_center)**2, axis=1)) \
+                                  for cluster_center in self.cluster_centers_], axis=1)
+        clusters = distances.argmin(axis=1)
+        return clusters
 
     def score(self, X, y):
-        pass
-
+        return (self.predict(X) == y).sum() / X.shape[0]
