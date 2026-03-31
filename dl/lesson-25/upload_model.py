c8b8ef5f25a9ae598721da655d5e1f6e723cf46b
Mardon <mardonbekhazratov@gmail.com>
2026-03-31 19:28:18 +0500

    update lesson 25

--- dl/lesson-25/upload_model.py
+++ dl/lesson-25/upload_model.py
@@ -1,10 +1,10 @@
 from huggingface_hub import HfApi
 api = HfApi()
 
-model_id = "mardonbekhazratov/bert-base-uncased-fine-tuned-gguf"
+model_id = "mardonbekhazratov/gpt2-trained-from-scratch-gguf"
 api.create_repo(model_id, exist_ok=True, repo_type="model")
 api.upload_file(
-    path_or_fileobj=r"bert-base-uncased-fine-tuned.gguf",
-    path_in_repo=r"bert-base-uncased-fine-tuned.gguf",
+    path_or_fileobj=r"gpt2-from-scratch.gguf",
+    path_in_repo=r"gpt2-from-scratch.gguf",
     repo_id=model_id,
 )
